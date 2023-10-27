3ed0e3f08fedfb4533054842090039da8d2da35c
Danish1790 <danish.asif.7190@gmail.com>
2023-10-26 22:46:04 -0700

    project template

---
+++ Asp.NetCoreUserRegister-Login/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetCoreUserRegister_Login.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
